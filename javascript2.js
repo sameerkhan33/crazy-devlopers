648196e676dabd64357494e084eb7c10916f1c14
sameerkhan33 <102024434+sameerkhan33@users.noreply.github.com>
2022-03-21 15:02:17 +0530

    initial commit

---
+++ javascript2.js
@@ -0,0 +1,15 @@
+function name(params) {
+    
+}
+setTimeout(() => {
+    // alert("Hi guys we are appreciate with you and kindly signup on our page")
+    console.log("Hey sign up please")
+}, 10000);
+let sum = ( HTML,html)=>{
+    return html.html
+}
+const searchFun = () =>{
+    let search = document.getElementById('myInput').nodeValue.toUpperCase();
+
+    let html = document.getElementsByName('html');
+}
